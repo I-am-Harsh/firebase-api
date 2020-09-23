10a4228f318590b7610d42fd48761825b5b330bd
Harsh Verma <verma.harsh98@gmail.com>
2020-09-23 20:24:39 +0530

    updated env variables

--- routes/upload.js
+++ routes/upload.js
@@ -6,13 +6,13 @@ var router = express.Router();
 /* GET home page. */
 
 const storage = new Storage({
-    keyFilename: "image-api-91bce-firebase-adminsdk-ldyiw-c1ca6e58c2.json",
-    projectId: "image-api-91bce"
+    keyFilename: process.env.keyFileName,
+    projectId: process.env.projectId
     // projectId:
 });
 // const storage = new Storage();
 
-const bucket = storage.bucket("image-api-91bce.appspot.com");
+const bucket = storage.bucket(process.env.bucket);
 
 const multer = Multer({
     storage: Multer.memoryStorage(),
@@ -20,11 +20,8 @@ const multer = Multer({
         fileSize: 5 * 1024 * 1024 // no larger than 5mb, you can change as needed.
     }
 });
-console.log(process.env.projectId);
-// const storageRef = storage.
 
 router
-
 .post('/', multer.single('image'), (req, res) => {
 	const file = req.file;
 	console.log('file recieved');
